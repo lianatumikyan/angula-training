88b2d881e2e954adc88924cd6979796046f85325
lianatumikyan <ltumikyan@gmail.com>
2018-11-16 01:01:40 +0400

    Fibonacci numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Subject} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'angular-training';
+export class AppComponent implements OnInit {
+  public subject = new Subject<number>();
+
+  fibonacci(number) {
+
+    let previous_first = 0, previous_second = 1, next = 1;
+
+    for(let i = 2; i <= number; i++) {
+      next = previous_first + previous_second;
+      previous_first = previous_second;
+      previous_second = next;
+      this.subject.next(next);
+    }
+    return next;
+  };
+
+  ngOnInit(): void {
+    this.subject.subscribe(num => {
+      console.log(num);
+    });
+    this.fibonacci(10);
+  }
 }
